b65e215883cbe9fadc3f91566ea2ef12fc966a1c
Alex Nagy <rebeloperalpar@gmail.com>
2023-03-22 12:49:27 +0200

    package fix

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         .target(
             name: "BFShared",
             dependencies: [
-                "FirebaseService300",
+                .product(name: "FirebaseService", package: "FirebaseService300"),
                 .product(name: "FirebaseFunctions", package: "Firebase"),
                 .product(name: "FirebaseDynamicLinks", package: "Firebase"),
                 .product(name: "FirebaseMessaging", package: "Firebase"),
